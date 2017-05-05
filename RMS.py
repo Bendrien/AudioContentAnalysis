1ac265841acd885a0195c15a3533a0bf302da744
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-05-05 17:30:49 +0200

    First python file.

---
+++ RMS.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scipy as sp
+from scipy import special, optimize
+import matplotlib.pyplot as plt
+
+def main():
+    x = sp.linspace(0, 10, 5000);
+    plt.plot(x);
+    plt.show();
+
+
+if(__name__ == '__main__'):
+    main()
