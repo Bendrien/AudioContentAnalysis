b7ce076d758f8724c002d3f819c25ba05d52498d
Maximilian Wagenbach <maximilian.wagenbach@native-instruments.de>
2017-05-25 19:20:27 +0200

    Added a nice plot for the spectral features.

--- Aufgabe 3/Aufgabe3.py
+++ Aufgabe 3/Aufgabe3.py
@@ -7,7 +7,7 @@ import math
 
 # Calculates the spectral rolloff
 # Returns the frequency below which 95% of the energy is located
-def spectralRolloff(frame, fs: int):
+def spectralRolloff(frame, previousFrame, fs: int):
     magnitudeSpectrum = getMagnitudeSpectrum(frame)
     percent = 0.95
     thresholdSum = percent * sum(i * i for i in magnitudeSpectrum)
@@ -24,43 +24,33 @@ def spectralRolloff(frame, fs: int):
 
 
 # Calculates the spectral centroid
-def spectralCentroid(frame, fs: int):
+def spectralCentroid(frame, previousFrame,  fs: int):
     magnitudeSpectrum = getMagnitudeSpectrum(frame)
     frameSize = len(frame)
     frequencies = np.abs(np.fft.fftfreq(frameSize, 1.0/fs)[:frameSize // 2 + 1])  # positive frequencies
     return sum(frequencies * magnitudeSpectrum) / sum(magnitudeSpectrum)
 
 
-# x:         Audio samples (list like)
-# fs:        Sample Rate
-# frameSize: Frame size in sample
-# hopSize:   Hop size in sample
-# zeroPad:   Enabled zero padding at the end to fit the frameSize
-def blockwise(x, fs: int, frameSize: int, hopSize: int, blockwiseFunction, functionArgs, zeroPad: bool = True):
+# x:            Audiosamples (list like)
+# fs:           Sample rate
+# frameSize:    Frame size in sample
+# hopSize:      Hop size in sample
+# filename:     Name of the file
+def blockwiseFeature(x, fs: int, frameSize: int, hopSize: int, filename: str):
+    rolloff = blockwise(x, frameSize, hopSize, spectralRolloff, [fs])
+    centroid = blockwise(x, frameSize, hopSize, spectralCentroid, [fs])
 
-    if (zeroPad):
-        # zero pad at the end
-        rest = len(x) % hopSize
-        if rest != 0:
-            remainder = frameSize - rest
-        else:
-            remainder = hopSize
-        x = np.lib.pad(x, (0, remainder), 'constant', constant_values=0)
-
-    # calculate the number of blocks
-    numberOfFrames = int(math.ceil(len(x) / hopSize))
-    output = []
-
-    if (zeroPad):
-        numberOfFrames -= 1  # minus 1, because we zero padded
-
-    # blockwise processing
-    for frameCount in range(0, numberOfFrames):
-        begin = int(frameCount * hopSize)
-        frame = x[begin: begin + frameSize]
-        output.append(blockwiseFunction(frame, *functionArgs))
-
-    return output
+    f, subplots = plt.subplots(3)
+    plt.subplots_adjust(hspace=0.5)
+    subplots[0].plot(rolloff)
+    subplots[0].set_title('Spectral rolloff der Datei ' + filename)
+    subplots[0].set_xlabel("Frames")
+    subplots[0].set_ylabel("Frequenz (Hz)")
+    subplots[1].plot(centroid)
+    subplots[1].set_title('Spectral centroid der Datei ' + filename)
+    subplots[1].set_xlabel("Frames")
+    subplots[1].set_ylabel("Frequenz (Hz)")
+    plt.show()
 
 
 def main():
@@ -76,8 +66,7 @@ def main():
 
     frameSize = 2048
     hopSize = frameSize // 2
-    plt.plot(blockwise(flute, fs, frameSize, hopSize, spectralCentroid, fs))
-    plt.show()
+    blockwiseFeature(flute, fs, frameSize, hopSize, "flute_1.wav")
 
     ## Aufgabe 1: get_f0
     #print("Aufgabe 1 - Test get_f0: Grundfrequenz " + str(round(get_f0(triangle, fs), 3)) + " Hz")
@@ -144,6 +133,42 @@ def msToSamples(timeInMs: int, fs: int):
 
 def getMagnitudeSpectrum(frame):
     return abs(np.fft.rfft(frame))
+
+
+# x:                 Audio samples (list like)
+# frameSize:         Frame size in sample
+# hopSize:           Hop size in sample
+# blockwiseFunction: The function that is to be called blockwise (first function parameter is the block)
+# functionArgs:      Additional argument the the blockwise function
+# zeroPad:           Enabled zero padding at the end to fit the frameSize
+def blockwise(x, frameSize: int, hopSize: int, blockwiseFunction, functionArgs, zeroPad: bool = True):
+
+    if zeroPad:
+        # zero pad at the end
+        rest = len(x) % hopSize
+        if rest != 0:
+            remainder = frameSize - rest
+        else:
+            remainder = hopSize
+        x = np.lib.pad(x, (0, remainder), 'constant', constant_values=0)
+
+    # calculate the number of blocks
+    numberOfFrames = int(math.ceil(len(x) / hopSize))
+    output = []
+    previousFrame = np.zeros(frameSize)
+
+
+    if zeroPad:
+        numberOfFrames -= 1  # minus 1, because we zero padded
+
+    # blockwise processing
+    for frameCount in range(0, numberOfFrames):
+        begin = int(frameCount * hopSize)
+        frame = x[begin: begin + frameSize]
+        output.append(blockwiseFunction(frame, previousFrame, *functionArgs))
+        previousFrame = frame
+
+    return output
 
 
 if __name__ == '__main__':
